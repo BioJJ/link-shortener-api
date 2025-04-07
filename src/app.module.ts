00cc2f6fc1012da4606402870c545b5765478894
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2025-04-06 21:50:52 -0400

    feat: init

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { DatabaseModule } from './database/database.module'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from '@hapi/joi'
+import { AuthModule } from './auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard'
+import { UsersModule } from './modules/users/users.module'
+import { UrlModule } from './modules/url/url.module';
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				DB_HOST: Joi.string().required(),
+				DB_PORT: Joi.number().required(),
+				DB_USER: Joi.string().required(),
+				DB_PASSWORD: Joi.string().required(),
+				DB_DB: Joi.string().required(),
+				PORT: Joi.number()
+			})
+		}),
+		DatabaseModule,
+		UsersModule,
+		AuthModule,
+		UrlModule,
+	],
+	controllers: [AppController],
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuard
+		}
+	]
+})
+export class AppModule {}
